71e21be7075c3526adffd95015b338a913394a25
Piradon <piradonleungamornnara@gmail.com>
2026-07-14 09:12:29 +0700

    feat: Implement Greeting component in App and add TypeScript types for props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return <Greeting name="John" age={20} />;
 }
 
 export default App;
